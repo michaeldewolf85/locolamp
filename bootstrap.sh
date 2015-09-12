f7524ce614de9abd4682222eb9469a8dff72a789
Michael DeWolf <michaeldewolf85@gmail.com>
2015-09-12 17:36:25 -0400

    Make web root directory name an argument passed into the provisioner from the Vagrantfile.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,4 +11,4 @@ apt-get install -y apache2 mysql-server php5 libapache2-mod-php5 php5-mysql
 
 mysql -uroot -e "CREATE DATABASE locolamp"
 rm -rf /var/www/html
-ln -s /vagrant/docroot /var/www/html
+ln -s /vagrant/${1} /var/www/html
